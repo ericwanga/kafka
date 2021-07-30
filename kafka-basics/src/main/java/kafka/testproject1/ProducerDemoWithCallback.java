6c2ca4d02d38c8179364774e8f3873030c453e6a
Eric Wang <84424184+ericwanga@users.noreply.github.com>
2021-07-30 18:24:25 +0930

    add kafka java projects not docker

---
+++ kafka-basics/src/main/java/kafka/testproject1/ProducerDemoWithCallback.java
@@ -0,0 +1,60 @@
+package kafka.testproject1;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemoWithCallback {
+    public static void main(String[] args) {
+
+        Logger logger = LoggerFactory.getLogger(ProducerDemoWithCallback.class);
+
+        String bootstrapServers = "127.0.0.1:9092";
+
+        // create Producer properties
+        Properties properties = new Properties();
+//        properties.setProperty("bootstrap.servers", bootstrapServers);
+//        properties.setProperty("key.serializer", "");
+//        properties.setProperty("value.serializer", "");
+
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // create the producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        for (int i=0; i<10; i++) {
+            // create a producer record
+            ProducerRecord<String, String> record =
+                    new ProducerRecord<>("first_topic", "hello world " + Integer.toString(i));
+
+            // send data - asynchronous (background, data is never sent only by this. Need flush)
+            producer.send(record, new Callback() {
+                @Override
+                public void onCompletion(RecordMetadata metadata, Exception exception) {
+                    // executes every time a record is sent or an exception is thrown
+                    if (exception == null) {
+                        // the record was successfully sent
+                        logger.info("Received new metadata. \n" +
+                                "Topic: " + metadata.topic() + "\n" +
+                                "Partition: " + metadata.partition() + "\n" +
+                                "Offset: " + metadata.offset() + "\n" +
+                                "Timestamp: " + metadata.timestamp());
+                    } else {
+                        // handle the exception
+                        logger.error("Error while producing ", exception);
+                    }
+                }
+            });
+        }
+
+        // flush data
+        producer.flush();
+        // flush and close producer
+        producer.close();
+    }
+}
